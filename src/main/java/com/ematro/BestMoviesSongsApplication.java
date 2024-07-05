9243a103be9784fe4d540c755b0b526e1ca31294
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-07-05 23:49:46 +0200

    Initial commit

---
+++ src/main/java/com/ematro/BestMoviesSongsApplication.java
@@ -0,0 +1,13 @@
+package com.ematro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestMoviesSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestMoviesSongsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ematro/BestMoviesSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ematro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestMoviesSongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
