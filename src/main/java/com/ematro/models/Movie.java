6b5a7eefbd38497decb2abd1806cfc4aa08cee36
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-07-06 01:09:02 +0200

    add: attributi e metodi nei modelli

--- src/main/java/com/ematro/models/Movie.java
+++ src/main/java/com/ematro/models/Movie.java
@@ -1,5 +1,23 @@
 package com.ematro.models;
 
 public class Movie {
+	
+	private int id;
+	private String title;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
