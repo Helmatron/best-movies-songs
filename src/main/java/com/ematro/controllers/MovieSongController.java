92a99842c140c1e906f57c22112c7cf1f7f8c2af
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-07-06 18:41:23 +0200

    alto mare

--- src/main/java/com/ematro/controllers/MovieSongController.java
+++ src/main/java/com/ematro/controllers/MovieSongController.java
@@ -1,18 +1,30 @@
 package com.ematro.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ematro.models.Movie;
 
 @Controller
 public class MovieSongController {
 	
+	
+	
 	@GetMapping("/songs")
-	public String songs() {
+	public String songs(String song, Model model) {
+		
+		
+		
 		return "songs";
 	};
 	
 	@GetMapping("/movies")
-	public String movies() {
+	public String movies(Model model) {
+		model.addAllAttributes(getBestMovies());
 		return "movies";
 	};
 }
--- src/main/java/com/ematro/models/Movie.java
+++ src/main/java/com/ematro/models/Movie.java
@@ -1,7 +1,10 @@
 package com.ematro.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Movie {
-	
+
 	private int id;
 	private String title;
 
@@ -20,4 +23,26 @@ public class Movie {
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
+	private List<Movie> getBestMovies() {
+		List<Movie> movies = new ArrayList<>();
+		
+		Movie movie1 = new Movie();
+		movie1.setId(1);
+		movie1.setTitle("Chi ha incastrato Roger Rabbit");
+		
+		Movie movie2 = new Movie();
+		movie2.setId(2);
+		movie2.setTitle("Big Fish");
+		
+		Movie movie3 = new Movie();
+		movie3.setId(3);
+		movie3.setTitle("La spada nella roccia");
+		
+		movies.add(movie1);
+		movies.add(movie2);
+		movies.add(movie3);
+		
+		return movies;
+	} 
 }
